8b59f5456a33709ee097ea24c842b9769c8450e3
Freddy Rios <freddyrios2000@gmail.com>
2025-07-16 12:30:24 -0600

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@/components/ui/button";
-import AnimatedBackground from "./components/AnimatedBackground";
 
 function App() {
   return (
     <div>
-      <AnimatedBackground />
       <div className="flex flex-col items-center justify-center min-h-svh">
         <Button>Click me</Button>
       </div>
